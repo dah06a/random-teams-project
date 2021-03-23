dd473282ae415a1f5247e6e6f6d1538674ace6d0
David Henry <dah06a@acu.edu>
2021-03-22 22:10:17 -0500

    Navigation options added and experimented.

--- components/Home.js
+++ components/Home.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, SafeAreaView, ScrollView } from 'react-native';
+import { ListItem } from 'react-native-elements';
 
 export default class Home extends Component {
     render() {
         return (
-            <View>
-                <Text>Home</Text>
-            </View>
+            <SafeAreaView style={{flex: 1}}>
+                <ScrollView>
+                    <ListItem>
+                        <ListItem.Content>
+                            <ListItem.Title><Text>Example List Item</Text></ListItem.Title>
+                        </ListItem.Content>
+                    </ListItem>
+                </ScrollView>
+            </SafeAreaView>
         );
     }
 }
--- components/Main.js
+++ components/Main.js
@@ -1,72 +1,54 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from './Home';
 import Create from './Create';
-
-const NavTabBar =({ state, descriptors, navigation }) => {
-    return (
-      <View style={{ flexDirection: 'row' }}>
-        {state.routes.map((route, index) => {
-          const { options } = descriptors[route.key];
-          const label =
-            options.tabBarLabel !== undefined
-              ? options.tabBarLabel
-              : options.title !== undefined
-              ? options.title
-              : route.name;
-
-          const isFocused = state.index === index;
-
-          const onPress = () => {
-            const event = navigation.emit({
-              type: 'tabPress',
-              target: route.key,
-            });
-
-            if (!isFocused && !event.defaultPrevented) {
-              navigation.navigate(route.name);
-            }
-          };
-
-          const onLongPress = () => {
-            navigation.emit({
-              type: 'tabLongPress',
-              target: route.key,
-            });
-          };
-
-          return (
-            <TouchableOpacity
-              accessibilityRole="button"
-              accessibilityState={isFocused ? { selected: true } : {}}
-              accessibilityLabel={options.tabBarAccessibilityLabel}
-              testID={options.tabBarTestID}
-              onPress={onPress}
-              onLongPress={onLongPress}
-              style={{ flex: 1, marginBottom: 50 }}
-            >
-              <Text style={{ color: isFocused ? '#673ab7' : '#222' }}>
-                {label}
-              </Text>
-            </TouchableOpacity>
-          );
-        })}
-      </View>
-    );
-  }
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
+
+function NavigationTabs() {
+  return (
+    <Tab.Navigator
+		initialRouteName="Make Teams"
+		screenOptions={{
+			activeTintColor: 'orange',
+		}}
+    >
+		<Tab.Screen
+			name="Make Teams"
+			component={Home}
+			options={{
+				tabBarLabel: 'Make Teams',
+				tabBarIcon: ({ color, size }) => (
+					<MaterialCommunityIcons name="home" color={'orange'} size={size} />
+				),
+				headerStyle: {
+					backgroundColor: 'orange',
+					height: 100
+				}
+			}}
+		/>
+		<Tab.Screen
+			name="Create"
+			component={Create}
+			options={{
+				tabBarLabel: 'Create',
+				tabBarIcon: ({ color, size }) => (
+					<MaterialCommunityIcons name="home" color={'orange'} size={size} />
+				),			}}
+		/>
+
+    </Tab.Navigator>
+  );
+}
 
 export default class Main extends Component {
     render() {
         return (
             <NavigationContainer>
-                <Tab.Navigator tabBar={props => <NavTabBar {...props} />}>
-                    <Tab.Screen name="Home" component={Home} />
-                    <Tab.Screen name="Create New" component={Create} />
-                </Tab.Navigator>
+                <NavigationTabs />
             </NavigationContainer>
         );
     }
